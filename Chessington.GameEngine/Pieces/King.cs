0dab69a1d6c7044e87ad7bae5acd7f36b0a84fe1
Luke Marrai <Luke.Marrai@softwire.com>
2018-11-12 15:28:56 +0000

    Made king able to move to adjacent squares.

--- Chessington.GameEngine/Pieces/King.cs
+++ Chessington.GameEngine/Pieces/King.cs
@@ -10,7 +10,19 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var availableMoves = new List<Square>();
+            var currentSquare = board.FindPiece(this);
+
+            for (int i=-1; i<=1;i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    availableMoves.Add(Square.At(currentSquare.Row+i,currentSquare.Col+j));
+                }
+            }
+    
+            availableMoves.Remove(Square.At(currentSquare.Row, currentSquare.Col));
+            return availableMoves;
         }
     }
 }
