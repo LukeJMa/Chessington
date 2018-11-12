84b98c9899cb7ecd40d1e520517021eeecd1ddd1
Luke Marrai <Luke.Marrai@softwire.com>
2018-11-12 14:25:59 +0000

    Made white pawns able to move to square at (7,0), black squares able to move to square at (2,0).

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Chessington.GameEngine.Pieces
@@ -10,7 +11,13 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            if (Player == Player.White)
+            {
+                return (IEnumerable<Square>) new List<Square> {new Square(6,0)};
+            }
+
+            return (IEnumerable<Square>)new List<Square> { new Square(2, 0) };
+
         }
     }
 }
