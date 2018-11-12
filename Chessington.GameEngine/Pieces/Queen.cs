369015323a70142a4e98169198e5303f4c199d34
Luke Marrai <Luke.Marrai@softwire.com>
2018-11-12 15:13:54 +0000

    Made queen able to move laterally and on the main diagonal (except stating position).

--- Chessington.GameEngine/Pieces/Queen.cs
+++ Chessington.GameEngine/Pieces/Queen.cs
@@ -10,7 +10,31 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var availableMoves = new List<Square>();
+            var currentSquare = board.FindPiece(this);
+
+            // Lateral movement.
+            for (var i = 0; i < 8; i++)
+                availableMoves.Add(Square.At(currentSquare.Row, i));
+
+            for (var i = 0; i < 8; i++)
+                availableMoves.Add(Square.At(i, currentSquare.Col));
+
+            // Diagonal movement.
+
+            //Checking the backwards diagonal, i.e. 0,0 1,1, 2,2
+            for (var i = 0; i < 8; i++)
+                availableMoves.Add(Square.At(i, i));
+
+            //Checking the forwards diagonal i.e. 5,3 6,2 7,1
+            for (var i = 1; i < 8; i++)
+                availableMoves.Add(Square.At(i, 8 - i));
+
+
+            //Get rid of our starting location.
+            availableMoves.RemoveAll(s => s == Square.At(currentSquare.Row, currentSquare.Col));
+
+            return availableMoves;
         }
     }
 }
