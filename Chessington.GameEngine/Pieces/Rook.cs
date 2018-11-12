85c3d08c02f669b6d8b005c36e255041f2c3796e
Luke Marrai <Luke.Marrai@softwire.com>
2018-11-12 16:28:56 +0000

    Made rook unable to move past pieces, and unable to move onto pieces of the same colour.

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -12,12 +12,82 @@ namespace Chessington.GameEngine.Pieces
         {
             var availableMoves = new List<Square>();
             var currentSquare = board.FindPiece(this);
+            
+            // right
+            for (var i = 1; currentSquare.Row + i < 8; i++)
+            {
+                if (board.GetPiece(Square.At(currentSquare.Row + i, currentSquare.Col)) == null)
+                {
+                    availableMoves.Add(Square.At(currentSquare.Row + i, currentSquare.Col));
+                }
+                else if (board.GetPiece(Square.At(currentSquare.Row+i, currentSquare.Col)).Player!=this.Player)
+                {
+                    availableMoves.Add(Square.At(currentSquare.Row + i, currentSquare.Col));
+                    break;
+                }
+                else
+                {
+                    break;
+                }
 
-            for (var i = 0; i < 8; i++)
-                availableMoves.Add(Square.At(currentSquare.Row, i));
+            }
 
-            for (var i = 0; i < 8; i++)
-                availableMoves.Add(Square.At(i, currentSquare.Col));
+            // left
+            for (var i = 1; currentSquare.Row - i >=0; i++)
+            {
+                if (board.GetPiece(Square.At(currentSquare.Row - i, currentSquare.Col)) == null)
+                {
+                    availableMoves.Add(Square.At(currentSquare.Row - i, currentSquare.Col));
+                }
+                else if (board.GetPiece(Square.At(currentSquare.Row - i, currentSquare.Col)).Player != this.Player)
+                {
+                    availableMoves.Add(Square.At(currentSquare.Row - i, currentSquare.Col));
+                    break;
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+
+            // up
+            for (var i = 1; currentSquare.Col + i < 8; i++)
+            {
+                if (board.GetPiece(Square.At(currentSquare.Row , currentSquare.Col+i)) == null)
+                {
+                    availableMoves.Add(Square.At(currentSquare.Row , currentSquare.Col+i));
+                }
+                else if (board.GetPiece(Square.At(currentSquare.Row , currentSquare.Col+i)).Player != this.Player)
+                {
+                    availableMoves.Add(Square.At(currentSquare.Row , currentSquare.Col+i));
+                    break;
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+
+            // down
+            for (var i = 1; currentSquare.Col - i>=0; i++)
+            {
+                if (board.GetPiece(Square.At(currentSquare.Row, currentSquare.Col - i)) == null)
+                {
+                    availableMoves.Add(Square.At(currentSquare.Row, currentSquare.Col - i));
+                }
+                else if (board.GetPiece(Square.At(currentSquare.Row, currentSquare.Col - i)).Player != this.Player)
+                {
+                    availableMoves.Add(Square.At(currentSquare.Row, currentSquare.Col - i));
+                    break;
+                }
+                else
+                {
+                    break;
+                }
+
+            }
 
             //Get rid of our starting location.
             availableMoves.RemoveAll(s => s == Square.At(currentSquare.Row, currentSquare.Col));
