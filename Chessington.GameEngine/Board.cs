85dc1245d0a7dd44b4344a665a03fee451547fc8
Luke Marrai <Luke.Marrai@softwire.com>
2018-11-13 12:24:45 +0000

    Added to Board.MoveTo to make it check if the movement is en passant, and if so the victim pawn is captured.

--- Chessington.GameEngine/Board.cs
+++ Chessington.GameEngine/Board.cs
@@ -58,6 +58,39 @@ namespace Chessington.GameEngine
                 OnPieceCaptured(board[to.Row, to.Col]);
             }
 
+            // Check if movement is en passant.
+            if (LastMoved != null
+                && movingPiece is Pawn
+                && LastMoved is Pawn
+                && LastMoved.Player != movingPiece.Player
+                && LastMoved.MoveCount == 1)
+            {
+                var lastMovedSquare = FindPiece(LastMoved);
+
+                // White movement
+                if (movingPiece.Player==Player.White
+                    && lastMovedSquare.Row == 3
+                    && from.Row == 3
+                    && to.Row==2
+                    && to.Col == lastMovedSquare.Col)
+                {
+                    OnPieceCaptured(LastMoved);
+                    board[lastMovedSquare.Row, lastMovedSquare.Col] = null;
+                }
+
+                // Black movement
+                if (movingPiece.Player == Player.Black
+                    && lastMovedSquare.Row == 4
+                    && from.Row == 4
+                    && to.Row ==5
+                    && to.Col == lastMovedSquare.Col)
+                {
+                    OnPieceCaptured(LastMoved);
+                    board[lastMovedSquare.Row, lastMovedSquare.Col] = null;
+                }
+
+            }
+
             //Move the piece and set the 'from' square to be empty.
             board[to.Row, to.Col] = board[from.Row, from.Col];
             board[from.Row, from.Col] = null;
